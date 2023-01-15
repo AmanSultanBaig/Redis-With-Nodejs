de80a0ac7ffe48a9cbc0fc1eaa011697e509782f
Aman Sultan Baig <amansultanbaig@gmail.com>
2023-01-15 17:07:08 +0500

    api added which will fetch 5000 photos

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
+
 const app = express();
 
 let port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello from Redis with node"))
+app.get('/photos', async (req, res) => {
+    const { data } = await axios.get(`${process.env.BASE_URL}/photos`);
+    res.send(data)
+})
 
 app.listen(port, () => console.log(`App is running on http://localhost:${port} `))
